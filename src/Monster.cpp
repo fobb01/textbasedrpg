e0b81043300df33e36228dbf850e9c6664696bf3
Fobb <you@example.com>
2019-08-23 16:03:42 -0600

    Removed redeclaration

--- src/Monster.cpp
+++ src/Monster.cpp
@@ -1,18 +1,2 @@
-#include
+#include "Monster.h"
 
-class Monster: public Mob
-{
-public:
-  // need constructor
-   Monster(int level, int hp, int mp, int ac)     //Calls the mob constructor
-  {
-    Mob(level, hp, mp, ac);
-  }
-
-  int* monsterLoc();          //Returns a pointer to the location of the monster on the board
-  
-private:
-	
-	
-
-};
