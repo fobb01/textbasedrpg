6b8ed40f407f0e0c7da62e1604df0d674a8c1266
Fobb <you@example.com>
2019-08-23 14:04:08 -0600

    Created a broken dungeon classs

--- src/headers/Dungeon.h
+++ src/headers/Dungeon.h
@@ -1,19 +1,19 @@
 #pragma once
 
 #include "Tile.h"
+#include <vector>
 
 class Dungeon{
 private:
-    Tile* level[200][200];                    //TODO: FIX THIS IT IS VERY BROKEN!!!
+    // Tile* level[200][200];                    //TODO: FIX THIS IT IS VERY BROKEN!!!
+
     bool buildLevel(int vertSize, int horiSize);                  //Builds the dungeon itself 
 
-    int vertical;
-    int horizontal;
     
 public:
     
     Dungeon(int vertSize, int horiSize);               //Calls the function create dungeon in constructor with 100 for both as default values if not passed
-  
+    
 
     bool displayLevel();
 
